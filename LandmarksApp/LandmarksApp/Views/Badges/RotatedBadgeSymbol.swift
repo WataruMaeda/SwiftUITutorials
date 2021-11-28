f3a3e4f4f3b7c1fa0acb14b2629328d0ad3f4b7d
WataruMaeda <w.maeda.jp@gmail.com>
2021-11-28 15:38:17 -0800

    chore: complete chapter 2

---
+++ LandmarksApp/LandmarksApp/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  LandmarksApp
+//
+//  Created by Wataru Maeda on 2021/11/28.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+  let angle: Angle
+  var body: some View {
+    BadgeSymbol()
+      .padding(-60)
+      .rotationEffect(angle, anchor: .bottom)
+  }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+  static var previews: some View {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SwiftUITutorials
+
+- [x] Chapter 1: SwiftUI Essentials
+- [x] Chapter 2: Drawing and Animation
+
+https://developer.apple.com/tutorials/swiftui
